e0ca7b9a80c21f0a675bb4ce0df9dd87c24d8537
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2023-03-01 14:55:08 +0330

    add transition and active item

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const contents = document.querySelectorAll('.section__content');
 
 circles.forEach((circle , index) => {
     circle.addEventListener('click' , () => {
+
         circle.classList.toggle('active');
         sections[index].classList.toggle('active');
-        contents[index].classList.toggle('active');
+
+        if (contents[index].style.maxHeight) {
+            contents[index].style.maxHeight = null;
+        } else {
+            contents[index].style.maxHeight = contents[index].scrollHeight + "px";
+        }
+
     })
+})
+
+contents.forEach((content , index) => {
+    if(content.classList.contains('active')){
+        content.style.maxHeight = 'fit-content';
+    }
 })
